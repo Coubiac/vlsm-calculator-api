4d7aa68d0752d02173b95ff0d228499f0492fa7a
Benoit GRISOT <benoit.grisot@gmail.com>
2021-02-17 09:59:21 +0100

    modification jacoco lombok

--- src/main/java/com/begr/vlsm/controller/MainController.java
+++ src/main/java/com/begr/vlsm/controller/MainController.java
@@ -4,17 +4,15 @@ import com.begr.vlsm.model.request.NetworkDetailRequestModel;
 import com.begr.vlsm.model.response.SubnetResponseModel;
 import com.begr.vlsm.service.VlsmCalculatorService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.*;
 import java.util.List;
-import java.util.Set;
+
 
 @RestController
-@Validated
 public class MainController {
 
     @PostMapping(value = "/")
--- src/test/java/com/begr/vlsm/VlsmApplicationTests.java
+++ src/test/java/com/begr/vlsm/VlsmApplicationTests.java
@@ -1,13 +1,21 @@
 package com.begr.vlsm;
 
+import com.begr.vlsm.controller.MainController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class VlsmApplicationTests {
 
+    @Autowired
+    MainController controller;
+
     @Test
     void contextLoads() {
+        assertThat(controller).isNotNull();
     }
 
 }
